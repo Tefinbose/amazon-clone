c61e8f3abd0388bc40a5c3b9bb91ae553783162d
Tefin bose <tefinbose2003@gmail.com>
2025-08-31 18:45:41 +0530

    Generate Html for Order Summary

--- javascript-amazon-project-main/data/Cart.js
+++ javascript-amazon-project-main/data/Cart.js
@@ -1,4 +1,13 @@
- export const Cart=[];
+ export const Cart=[
+    {ProductId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity:1
+    },
+    {
+        ProductId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity:1
+    }
+    
+ ];
 console.log(Cart);
 
 // Fetching catProducts
